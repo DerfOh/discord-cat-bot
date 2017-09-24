d9f7a0765c6e2edcc6d5ff6e3a8629466781fcae
DerfOh <fredrick.p@outlook.com>
2017-09-24 04:50:09 +0100

    Initial commit, create all the files.

---
+++ bot/bot.go
@@ -0,0 +1,90 @@
+package bot
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/derfoh/cat-bot/commands"
+	"github.com/derfoh/cat-bot/config"
+)
+
+// BotID is the id set by discord in the main function
+var BotID string
+var goBot *discordgo.Session
+
+// Start starts opens connections and starts the bot
+func Start() {
+	// connect
+	goBot, err := discordgo.New("Bot " + config.Token)
+	checkExit(err)
+
+	// get bot info and set bot id with user info
+	u, err := goBot.User("@me")
+	checkLog(err)
+
+	BotID = u.ID
+
+	// Add handlers
+	goBot.AddHandler(messageHandler)
+
+	// Open connection
+	err = goBot.Open()
+	checkExit(err)
+
+	// Log bot is running
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.\nPress CTRL-C to exit.")
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// check for prefix
+	if strings.HasPrefix(m.Content, config.BotPrefix) {
+		// ignore the bots messages
+		if m.Author.ID == BotID {
+			return
+		}
+
+		// if the message contains the string then do the thing
+		if m.Content == "!cat" {
+			response := cat.Cat()
+			s.ChannelMessageSend(m.ChannelID, response)
+		}
+
+		if m.Content == "!time" {
+			t := time.Now()
+			s.ChannelMessageSend(m.ChannelID, "The time is "+t.Format("15:04")+" in catsville.")
+		}
+
+		if m.Content == "!date" {
+			t := time.Now()
+			s.ChannelMessageSend(m.ChannelID, "The date is "+t.Format("01-02-2006")+" in catsville.")
+		}
+
+		// print message content
+		fmt.Println(m.Content)
+	}
+
+}
+
+// Stop ends closes the connection
+func Stop() {
+	// Cleanly close down the Discord session.
+	goBot.Close()
+}
+
+// basic error checker for go, logs then keeps running
+func checkLog(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+// basic error checker for go, logs then exits
+func checkExit(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
---
+++ commands/cat.go
@@ -0,0 +1,39 @@
+package cat
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type catpicStruct struct {
+	Link string `json:"file"`
+}
+
+var catpic = catpicStruct{}
+
+func Cat() string {
+	resp, err := http.Get("http://random.cat/meow")
+	checkLog(err)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	json.Unmarshal(body, &catpic)
+	return catpic.Link
+}
+
+// basic error checker for go, logs then keeps running
+func checkLog(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+// basic error checker for go, logs then exits
+func checkExit(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
