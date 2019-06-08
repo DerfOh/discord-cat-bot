dc78319a30c3adf8aaaacda58f84157f78f4f9f9
Fredrick Paulin <fredrick.p@outlook.com>
2019-06-07 20:19:36 -0400

    Add start/stop server commands files

---
+++ commands/startserver.go
@@ -0,0 +1,47 @@
+package command
+
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+//StartServer brings up a digital ocean server
+func StartServer(content []string, s *discordgo.Session, m *discordgo.MessageCreate) {
+	var droplet string
+	host := content[1]
+
+	switch host {
+	case "minecraft":
+		droplet = "145416705"
+	default:
+		response := "Unable to determine server ID"
+		s.ChannelMessageSend(m.ChannelID, response)
+		return
+	}
+
+	url := "https://api.digitalocean.com/v2/droplets/" + droplet + "/actions"
+	//fmt.Println("URL:>", url)
+	var bearer = "Bearer " + "93fec65a8a9669e4c2b6748bde1ba1124be2950e3f80954230add997c699766d"
+	var jsonStr = []byte(`{"type":"power_on"}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", bearer)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// fmt.Println("response Status:", resp.Status)
+	// fmt.Println("response Headers:", resp.Header)
+	// body, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println("response Body:", string(body))
+	response := resp.Status
+	s.ChannelMessageSend(m.ChannelID, response)
+}
+
+//curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer b7d03a6947b217efb6f3ec3bd3504582" -d '{"type":"power_off"}' "https://api.digitalocean.com/v2/droplets/3164450/actions"
