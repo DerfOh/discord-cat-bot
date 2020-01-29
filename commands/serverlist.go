1f3cad4428556b6852f597c71c237ea95d4fa5a9
Fredrick Paulin <fredrick.p@outlook.com>
2020-01-28 19:19:52 -0500

    Remove digital ocean PAT from comment in code

--- commands/serverlist.go
+++ commands/serverlist.go
@@ -12,79 +12,80 @@ import (
 	config "github.com/derfoh/discord-cat-bot/config"
 )
 
+// DropletList defines the structure for the json object returned by digital ocean api calls
+type DropletList struct {
+	Droplets []struct {
+		ID               int           `json:"id"`
+		Name             string        `json:"name"`
+		Memory           int           `json:"memory"`
+		Vcpus            int           `json:"vcpus"`
+		Disk             int           `json:"disk"`
+		Locked           bool          `json:"locked"`
+		Status           string        `json:"status"`
+		Kernel           interface{}   `json:"kernel"`
+		CreatedAt        time.Time     `json:"created_at"`
+		Features         []interface{} `json:"features"`
+		BackupIds        []interface{} `json:"backup_ids"`
+		NextBackupWindow interface{}   `json:"next_backup_window"`
+		SnapshotIds      []int         `json:"snapshot_ids"`
+		Image            struct {
+			ID            int           `json:"id"`
+			Name          string        `json:"name"`
+			Distribution  string        `json:"distribution"`
+			Slug          interface{}   `json:"slug"`
+			Public        bool          `json:"public"`
+			Regions       []string      `json:"regions"`
+			CreatedAt     time.Time     `json:"created_at"`
+			MinDiskSize   int           `json:"min_disk_size"`
+			Type          string        `json:"type"`
+			SizeGigabytes float64       `json:"size_gigabytes"`
+			Description   string        `json:"description"`
+			Tags          []interface{} `json:"tags"`
+			Status        string        `json:"status"`
+		} `json:"image"`
+		VolumeIds []interface{} `json:"volume_ids"`
+		Size      struct {
+			Slug         string   `json:"slug"`
+			Memory       int      `json:"memory"`
+			Vcpus        int      `json:"vcpus"`
+			Disk         int      `json:"disk"`
+			Transfer     float64  `json:"transfer"`
+			PriceMonthly float64  `json:"price_monthly"`
+			PriceHourly  float64  `json:"price_hourly"`
+			Regions      []string `json:"regions"`
+			Available    bool     `json:"available"`
+		} `json:"size"`
+		SizeSlug string `json:"size_slug"`
+		Networks struct {
+			V4 []struct {
+				IPAddress string `json:"ip_address"`
+				Netmask   string `json:"netmask"`
+				Gateway   string `json:"gateway"`
+				Type      string `json:"type"`
+			} `json:"v4"`
+			V6 []interface{} `json:"v6"`
+		} `json:"networks"`
+		Region struct {
+			Name      string   `json:"name"`
+			Slug      string   `json:"slug"`
+			Features  []string `json:"features"`
+			Available bool     `json:"available"`
+			Sizes     []string `json:"sizes"`
+		} `json:"region"`
+		Tags []interface{} `json:"tags"`
+	} `json:"droplets"`
+	Links struct {
+	} `json:"links"`
+	Meta struct {
+		Total int `json:"total"`
+	} `json:"meta"`
+}
+
 // ServerList returns a list of servers associated with a personal access token
 func ServerList(content []string, s *discordgo.Session, m *discordgo.MessageCreate) {
-	type DropletList struct {
-		Droplets []struct {
-			ID               int           `json:"id"`
-			Name             string        `json:"name"`
-			Memory           int           `json:"memory"`
-			Vcpus            int           `json:"vcpus"`
-			Disk             int           `json:"disk"`
-			Locked           bool          `json:"locked"`
-			Status           string        `json:"status"`
-			Kernel           interface{}   `json:"kernel"`
-			CreatedAt        time.Time     `json:"created_at"`
-			Features         []interface{} `json:"features"`
-			BackupIds        []interface{} `json:"backup_ids"`
-			NextBackupWindow interface{}   `json:"next_backup_window"`
-			SnapshotIds      []int         `json:"snapshot_ids"`
-			Image            struct {
-				ID            int           `json:"id"`
-				Name          string        `json:"name"`
-				Distribution  string        `json:"distribution"`
-				Slug          interface{}   `json:"slug"`
-				Public        bool          `json:"public"`
-				Regions       []string      `json:"regions"`
-				CreatedAt     time.Time     `json:"created_at"`
-				MinDiskSize   int           `json:"min_disk_size"`
-				Type          string        `json:"type"`
-				SizeGigabytes float64       `json:"size_gigabytes"`
-				Description   string        `json:"description"`
-				Tags          []interface{} `json:"tags"`
-				Status        string        `json:"status"`
-			} `json:"image"`
-			VolumeIds []interface{} `json:"volume_ids"`
-			Size      struct {
-				Slug         string   `json:"slug"`
-				Memory       int      `json:"memory"`
-				Vcpus        int      `json:"vcpus"`
-				Disk         int      `json:"disk"`
-				Transfer     float64  `json:"transfer"`
-				PriceMonthly float64  `json:"price_monthly"`
-				PriceHourly  float64  `json:"price_hourly"`
-				Regions      []string `json:"regions"`
-				Available    bool     `json:"available"`
-			} `json:"size"`
-			SizeSlug string `json:"size_slug"`
-			Networks struct {
-				V4 []struct {
-					IPAddress string `json:"ip_address"`
-					Netmask   string `json:"netmask"`
-					Gateway   string `json:"gateway"`
-					Type      string `json:"type"`
-				} `json:"v4"`
-				V6 []interface{} `json:"v6"`
-			} `json:"networks"`
-			Region struct {
-				Name      string   `json:"name"`
-				Slug      string   `json:"slug"`
-				Features  []string `json:"features"`
-				Available bool     `json:"available"`
-				Sizes     []string `json:"sizes"`
-			} `json:"region"`
-			Tags []interface{} `json:"tags"`
-		} `json:"droplets"`
-		Links struct {
-		} `json:"links"`
-		Meta struct {
-			Total int `json:"total"`
-		} `json:"meta"`
-	}
 
 	url := "https://api.digitalocean.com/v2/droplets"
 
-	// var bearer = "Bearer " + "93fec65a8a9669e4c2b6748bde1ba1124be2950e3f80954230add997c699766d"
 	var bearer = "Bearer " + config.DigitalOceanToken
 	var jsonStr = []byte(`{}`)
 	req, err := http.NewRequest("GET", url, bytes.NewBuffer(jsonStr))
